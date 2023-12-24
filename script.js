38003dbaea43d220993e9810301cb5971a4e6f5c
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-12-24 11:05:31 +0900

    Sammi.js.starting.data type

---
+++ script.js
@@ -0,0 +1,45 @@
+// alert("Hello Fozil")
+
+// "use strict"
+
+// number = 11;
+// console.log(number);
+
+let number = 5.6; // data type number
+
+console.log(-8/0); // infinity
+console.log(fozil*8); //NaN
+
+//string
+
+const clientName = "Fozil";
+const clientNamebb = `Fozil`;
+
+// boolean
+
+const ismarried = false;
+ const isCriccleEarth = true;
+
+ // null
+
+ console.log(clientAge);
+
+ //undefined
+ let und;
+ console.log(und);
+
+ //Object 
+ const thief = {
+    //key: value
+    name:Fozil,
+    age: 28,
+    isLong: false
+ }
+
+ console.log(thief.key);
+
+ //array
+
+ const color = ["red", "black", "green", 11, [], {}]
+
+ console.log(color[1]);
