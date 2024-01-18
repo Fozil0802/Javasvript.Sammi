2f134998f3e2126f7fe6777aab737294edc48da0
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2024-01-18 22:49:27 +0900

    Data type

--- script.js
+++ script.js
@@ -1,45 +1,96 @@
-// alert("Hello Fozil")
+// // alert("Hello Fozil")
 
-// "use strict"
+// // "use strict"
 
-// number = 11;
-// console.log(number);
+// // number = 11;
+// // console.log(number);
 
-let number = 5.6; // data type number
+// let number = 5.6; // data type number
 
-console.log(-8/0); // infinity
-console.log(fozil*8); //NaN
+// // console.log(-8/0); // infinity
+// // console.log(fozil*8); //NaN
+
+// //string
+
+// const clientName = "Fozil";
+// const clientNamebb = `Fozil`;
+
+// // boolean
+
+// const ismarried = false;
+//  const isCriccleEarth = true;
+
+//  // null
+
+// //  console.log(clientAge);
+
+//  //undefined
+//  let und;
+//  console.log(und);
+
+//  //Object
+//  const thief = {
+//     //key: value
+//     name:Fozil,
+//     age: 28,
+//     isLong: false
+//  }
+
+//  console.log(thief.key);
+
+//  //array
+
+//  const color = ["red", "black", "green", 11, [], {}]
+
+//  console.log(color[1]);
+
+//  number
+
+let num = 5.6; //data type => number
+
+console.log(8 / 0); //=> infinity
+console.log(-8 / 0); // => -infinity
+console.log(0 / -8); // 0
+console.log("fozil" * 5); // => NAN
 
 //string
 
-const clientName = "Fozil";
-const clientNamebb = `Fozil`;
+const name1 = "fozil 1995";
+const name2 = "fozil 1995";
+const name3 = `fozil 1995`;
 
-// boolean
+// Boolean
 
-const ismarried = false;
- const isCriccleEarth = true;
+const isMarried = false;
 
- // null
+//true => 1, ha
+//false => 0, yuq
 
- console.log(clientAge);
+// Null
 
- //undefined
- let und;
- console.log(und);
+// console.log(yuq uzgaruvchini chaqirish);
 
- //Object 
- const thief = {
-    //key: value
-    name:Fozil,
-    age: 28,
-    isLong: false
- }
+// Undefined
 
- console.log(thief.key);
+let benom;
 
- //array
+console.log(benom);
 
- const color = ["red", "black", "green", 11, [], {}]
+//object
 
- console.log(color[1]);
+const thief = {
+ //key = value   
+  name: "fozil",
+  age: 28,
+  islong: false,
+};
+console.log(thief);
+console.log(thief["age"]);
+
+//Array
+
+const colors = [
+  // 0        1      2   3  4  
+    "red", "black", 95, [], {}
+]
+console.log(colors[0]);
